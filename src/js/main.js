473e4376ded36c4dbf7d99797c81bdcc5c85cd16
hogashi <azumath2357_ginkaku314@yahoo.co.jp>
2015-12-09 01:32:03 +0900

    ver.1.6.1  Coped with the change of photo-tweet showing in twitter (again)

--- src/js/main.js
+++ src/js/main.js
@@ -14,9 +14,9 @@ document.addEventListener("click", start);
 
 function INCL(){
     // .permalink-tweet-container: ツイート詳細ページのメインツイート
-    // .OldMedia-photoContainer: 画像
+    // .AdaptiveMedia-photoContainer: 画像
     if(!!document.querySelector('.permalink-tweet-container')){
-        var mediatags = document.querySelector('.permalink-tweet-container').getElementsByClassName('OldMedia-photoContainer');
+        var mediatags = document.querySelector('.permalink-tweet-container').getElementsByClassName('AdaptiveMedia-photoContainer');
         for(var i=mediatags.length-1; i>=0; i--){
             var imgurl = mediatags[i].getElementsByTagName('img')[0].src;
              // if 画像URLが取得できたなら
@@ -31,7 +31,7 @@ function start(){
     // if まだ処理を行っていないなら
     if(!document.getElementById("twioriginput")){
          // if ツイート詳細ページかつメインツイートが画像ツイートなら
-        if(!!document.querySelector('.permalink-tweet-container .OldMedia')){
+        if(!!document.querySelector('.permalink-tweet-container')){
             // メインツイートの操作ボタン
             var sel = document.querySelector(".permalink-tweet-container .ProfileTweet-actionList");
             var divch = document.createElement("div");
