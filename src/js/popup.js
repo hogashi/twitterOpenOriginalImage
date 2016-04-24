f1cb8596dafac51d46801cedef55df4c2cbfc8ba
hogashi <azumath2357_ginkaku314@yahoo.co.jp>
2016-04-24 15:15:00 +0900

    revise saving behavior

--- src/js/popup.js
+++ src/js/popup.js
@@ -3,6 +3,7 @@ var showOnTweetDetail = document.getElementById('SHOW_ON_TWEET_DETAIL');
 var showOnTimeline = document.getElementById('SHOW_ON_TIMELINE');
 var openWithKeyPress = document.getElementById('OPEN_WITH_KEY_PRESS');
 var showOnTweetdeckTimeline = document.getElementById('SHOW_ON_TWEETDECK_TIMELINE');
+var stripImageSuffix = document.getElementById('STRIP_IMAGE_SUFFIX');
 
 window.onload = function(){
 	// 最初はどっちも機能オンであってほしい
@@ -31,6 +32,12 @@ window.onload = function(){
 	else {
 		showOnTweetdeckTimeline.checked = false;
 	}
+	if(localStorage['STRIP_IMAGE_SUFFIX'] != 'isfalse') {
+		stripImageSuffix.checked = true;
+	}
+	else {
+		stripImageSuffix.checked = false;
+	}
 	// document.getElementById('res').innerHTML = showOnTweetDetail.checked.toString() + showOnTimeline.checked.toString() + ':' + localStorage['SHOW_ON_TWEET_DETAIL'] + localStorage['SHOW_ON_TIMELINE'];
 }
 
@@ -39,6 +46,7 @@ document.getElementById('save').onclick = function() {
 	localStorage['SHOW_ON_TIMELINE'] = 'is' + (showOnTimeline.checked.toString());
 	localStorage['OPEN_WITH_KEY_PRESS'] = 'is' + (openWithKeyPress.checked.toString());
 	localStorage['SHOW_ON_TWEETDECK_TIMELINE'] = 'is' + (showOnTweetdeckTimeline.checked.toString());
+	localStorage['STRIP_IMAGE_SUFFIX'] = 'is' + (stripImageSuffix.checked.toString());
 	chrome.tabs.query({}, function(result) {
 		console.log(result);
 		for(i=0; i<result.length; i++) {
