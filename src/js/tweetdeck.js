c7044c8b8990983f4cd343a62bf3460eeaa33469
hogas <hogas@mma.club.uec.ac.jp>
2017-02-27 01:00:32 +0900

    WIP replace tab to spaces, add config-key

--- src/js/tweetdeck.js
+++ src/js/tweetdeck.js
@@ -4,7 +4,8 @@
 // 有効だった場合はDOMが変更される間に設定が読み込まれて有効になる
 // 無効だった場合はそのままボタンは表示されない
 let options = {
-	'SHOW_ON_TWEETDECK_TIMELINE': 'isfalse'
+  'SHOW_ON_TWEETDECK_TIMELINE': 'isfalse',
+  'SHOW_ON_TWEETDECK_TWEET_DETAIL': 'isfalse',
 }
 
 // ページ全体でDOMの変更を検知し都度ボタン設置
@@ -17,99 +18,99 @@ observer.observe(target, config)
 updateOptions()
 // 設定反映のためのリスナー設置
 chrome.runtime.onMessage.addListener(
-	function(request, sender, sendResponse) {
-		switch(request.method) {
-			case 'OPTION_UPDATED':
-				updateOptions()
-				sendResponse({data: 'done'})
-				break
-			default:
-				sendResponse({data: 'yet'})
-				break
-		}
-	}
+  function(request, sender, sendResponse) {
+    switch(request.method) {
+      case 'OPTION_UPDATED':
+        updateOptions()
+        sendResponse({data: 'done'})
+        break
+      default:
+        sendResponse({data: 'yet'})
+        break
+    }
+  }
 )
 
 // エラーメッセージの表示(予期せぬ状況の確認)
 function printException(tooiException) {
-	console.log('tooitd: ' + tooiException)
+  console.log('tooitd: ' + tooiException)
 }
 
 // 設定項目更新
 function updateOptions() {
-	// console.log('upOpt bfr: ' + options['SHOW_ON_TWEETDECK_TIMELINE']) // debug
-	Object.keys(options).map((key, i) => {
-		(function(k) {
-			chrome.runtime.sendMessage({method: 'GET_LOCAL_STORAGE', key: k},
-				function(response) {
-					options[k] = response.data
-					// 設定を読み込んだら機能を呼び出す
-					// 設定読込と同スコープに書くことで同期的に呼び出し
-					doTask()
-				}
-			)
-		})(key)
-	})
-	// console.log('upOpt bfr: ' + options['SHOW_ON_TWEETDECK_TIMELINE']) // debug
+  // console.log('upOpt bfr: ' + options['SHOW_ON_TWEETDECK_TIMELINE']) // debug
+  Object.keys(options).map((key, i) => {
+    (function(k) {
+      chrome.runtime.sendMessage({method: 'GET_LOCAL_STORAGE', key: k},
+        function(response) {
+          options[k] = response.data
+          // 設定を読み込んだら機能を呼び出す
+          // 設定読込と同スコープに書くことで同期的に呼び出し
+          doTask()
+        }
+      )
+    })(key)
+  })
+  // console.log('upOpt bfr: ' + options['SHOW_ON_TWEETDECK_TIMELINE']) // debug
 } // updateOptions end
 
 // 各機能の呼び出し
 function doTask() {
-	// console.log('upOpt bfr: ' + options['SHOW_ON_TWEETDECK_TIMELINE']) // debug
-	// if タイムラインにボタン表示する設定がされていたら
-	if(options['SHOW_ON_TWEETDECK_TIMELINE'] != 'isfalse') {
-		setButtonOnTimeline()
-	}
-	// // if ツイート詳細にボタン表示する設定がされていたら
-	// if(options['SHOW_ON_TWEET_DETAIL'] != 'isfalse') {
-	// 	setButtonOnTweetDetail()
-	// }
+  // console.log('upOpt bfr: ' + options['SHOW_ON_TWEETDECK_TIMELINE']) // debug
+  // if タイムラインにボタン表示する設定がされていたら
+  if(options['SHOW_ON_TWEETDECK_TIMELINE'] != 'isfalse') {
+    setButtonOnTimeline()
+  }
+  // // if ツイート詳細にボタン表示する設定がされていたら
+  // if(options['SHOW_ON_TWEET_DETAIL'] != 'isfalse') {
+  //   setButtonOnTweetDetail()
+  // }
 } // doTask end
 
 // エレメントへのstyle属性の設定
 function setStyle(e, attrs) {
-	Object.keys(attrs).map((key, i) => {
-		e.style[key] = attrs[key]
-	})
+  Object.keys(attrs).map((key, i) => {
+    e.style[key] = attrs[key]
+  })
 }
 
 // タイムラインにボタン表示
 function setButtonOnTimeline() {
-	// if タイムラインのツイートを取得できたら
-	// is-actionable: タイムラインのみ
-	if(document.getElementsByClassName('js-stream-item is-actionable').length != 0) {
-		// 各ツイートに対して
-		Array.from(document.getElementsByClassName('js-stream-item is-actionable'))
-			.map((tweet, i) => {
-			// if メディアツイート
-			// かつ メディアが画像(動画でもGIFでもない)
-			// かつ まだ処理を行っていないなら
-			if(!!(tweet.getElementsByClassName('js-media-image-link')[0])
-			 && !(tweet.getElementsByClassName('is-video')[0])
-			 && !(tweet.getElementsByClassName('is-gif')[0])
-			 && !(tweet.getElementsByClassName('tooiATimeline')[0])) {
-				// ボタンを設置
-				let origButton = document.createElement('a')
-				tweet.getElementsByTagName('footer')[0].appendChild(origButton)
-				// tweetdeckのツイート右上の時刻などに使われているclassを使う
-				// 設置の有無の判別用に'tooiATimeline'を付与する
-				origButton.className = 'pull-left margin-txs txt-mute tooiATimeline'
-				// 枠線の色は'Original'と同じく'.txt-mute'の色を使うのでボタンから取得して設定する
-				let borderColor = document.defaultView.getComputedStyle(origButton, '').color
-				// ボタンのスタイル設定
-				setStyle(origButton,
-					{border: `1px solid ${borderColor}`,
-					borderRadius: '2px',
-					fontSize: '0.75em',
-					marginLeft: '3px',
-					lineHeight: '1.5em',
-					paddingRight: '1px'})
-				origButton.insertAdjacentHTML('beforeend', 'Original')
-				// ボタンを押した時の挙動を設定する
-				origButton.addEventListener('click', openFromTimeline)
-			}
-		})
-	}
+  // if タイムラインのツイートを取得できたら
+  // is-actionable: タイムラインのみ
+  if(document.getElementsByClassName('js-stream-item is-actionable').length != 0) {
+    // 各ツイートに対して
+    Array.from(document.getElementsByClassName('js-stream-item is-actionable'))
+      .map((tweet, i) => {
+      // if メディアツイート
+      // かつ メディアが画像(動画でもGIFでもない)
+      // かつ まだ処理を行っていないなら
+      if(!!(tweet.getElementsByClassName('js-media-image-link')[0])
+       && !(tweet.getElementsByClassName('is-video')[0])
+       && !(tweet.getElementsByClassName('is-gif')[0])
+       && !(tweet.getElementsByClassName('tooiATimeline')[0])) {
+        // ボタンを設置
+        let origButton = document.createElement('a')
+        tweet.getElementsByTagName('footer')[0].appendChild(origButton)
+        // tweetdeckのツイート右上の時刻などに使われているclassを使う
+        // 設置の有無の判別用に'tooiATimeline'を付与する
+        origButton.className = 'pull-left margin-txs txt-mute tooiATimeline'
+        // 枠線の色は'Original'と同じく'.txt-mute'の色を使うのでボタンから取得して設定する
+        let borderColor = document.defaultView.getComputedStyle(origButton, '').color
+        // ボタンのスタイル設定
+        setStyle(origButton,
+          {border: `1px solid ${borderColor}`,
+          borderRadius: '2px',
+          fontSize: '0.75em',
+          marginLeft: '3px',
+          lineHeight: '1.5em',
+          paddingRight: '1px'})
+        origButton.insertAdjacentHTML('beforeend', 'Original')
+        // ボタンを押した時の挙動を設定する
+        origButton.addEventListener('click', openFromTimeline)
+      }
+    })
+  }
 } // setButtonOnTimeline end
 
 // // ツイート詳細にボタン表示
@@ -118,20 +119,20 @@ function setButtonOnTimeline() {
 
 // タイムラインから画像を新しいタブに開く
 function openFromTimeline(e) {
-	// イベント(MouseEvent)による既定の動作をキャンセル
-	e.preventDefault()
-	// イベント(MouseEvent)の親要素への伝播を停止
-	e.stopPropagation()
-	// ツイートの画像の親エレメントを取得するためにその親まで遡る
-	// if 上述のエレメントが取得できたら
-	if(this.parentNode.parentNode.parentNode.parentNode.getElementsByClassName('js-media')) {
-		openImagesInNewTab(
-			this.parentNode.parentNode.parentNode.parentNode.getElementsByClassName('js-media-image-link')
-		)
-	}
-	else {
-		printException('CANT_FIND_IMAGE_ELEMENT_ON_TIMELINE')
-	}
+  // イベント(MouseEvent)による既定の動作をキャンセル
+  e.preventDefault()
+  // イベント(MouseEvent)の親要素への伝播を停止
+  e.stopPropagation()
+  // ツイートの画像の親エレメントを取得するためにその親まで遡る
+  // if 上述のエレメントが取得できたら
+  if(this.parentNode.parentNode.parentNode.parentNode.getElementsByClassName('js-media')) {
+    openImagesInNewTab(
+      this.parentNode.parentNode.parentNode.parentNode.getElementsByClassName('js-media-image-link')
+    )
+  }
+  else {
+    printException('CANT_FIND_IMAGE_ELEMENT_ON_TIMELINE')
+  }
 } // openFromTimeline end
 
 // // ツイート詳細から画像を新しいタブに開く
@@ -140,14 +141,14 @@ function openFromTimeline(e) {
 
 // 画像を原寸で新しいタブに開く
 function openImagesInNewTab(tag) {
-	Array.from(tag).reverse().map((v, i) => {
-		let imgurl = v.style.backgroundImage
-		// if 画像URLが取得できたなら
-		if(!!imgurl) {
-			window.open(imgurl.replace(/^[^(]+\(\"(https:[^:]+)(|:[^:]+)\"\)$/, '$1:orig'))
-		}
-		else {
-			printException('CANT_FIND_IMAGE_URL')
-		}
-	})
+  Array.from(tag).reverse().map((v, i) => {
+    let imgurl = v.style.backgroundImage
+    // if 画像URLが取得できたなら
+    if(!!imgurl) {
+      window.open(imgurl.replace(/^[^(]+\(\"(https:[^:]+)(|:[^:]+)\"\)$/, '$1:orig'))
+    }
+    else {
+      printException('CANT_FIND_IMAGE_URL')
+    }
+  })
 }
