27108c02698356d60f714dc597afeec8fbb9b3e3
hogas <hogas@mma.club.uec.ac.jp>
2017-03-06 00:04:30 +0900

    add original-button to tweet-detail on tweetdeck

--- src/js/tweetdeck.js
+++ src/js/tweetdeck.js
@@ -57,14 +57,14 @@ function updateOptions() {
 // 各機能の呼び出し
 function doTask() {
   // console.log('upOpt bfr: ' + options['SHOW_ON_TWEETDECK_TIMELINE']) // debug
-  // if タイムラインにボタン表示する設定がされていたら
+  // if Tweetdeckのタイムラインにボタン表示する設定がされていたら
   if(options['SHOW_ON_TWEETDECK_TIMELINE'] != 'isfalse') {
-    setButtonOnTimeline()
+    setButtonOnTweetdeckTimeline()
   }
-  // // if ツイート詳細にボタン表示する設定がされていたら
-  // if(options['SHOW_ON_TWEET_DETAIL'] != 'isfalse') {
-  //   setButtonOnTweetDetail()
-  // }
+  // if Tweetdeckのツイート詳細にボタン表示する設定がされていたら
+  if(options['SHOW_ON_TWEETDECK_TWEET_DETAIL'] != 'isfalse') {
+    setButtonOnTweetdeckTweetDetail()
+  }
 } // doTask end
 
 // エレメントへのstyle属性の設定
@@ -75,7 +75,7 @@ function setStyle(e, attrs) {
 }
 
 // タイムラインにボタン表示
-function setButtonOnTimeline() {
+function setButtonOnTweetdeckTimeline() {
   // if タイムラインのツイートを取得できたら
   // is-actionable: タイムラインのみ
   if(document.getElementsByClassName('js-stream-item is-actionable').length != 0) {
@@ -88,13 +88,13 @@ function setButtonOnTimeline() {
       if(!!(tweet.getElementsByClassName('js-media-image-link')[0])
        && !(tweet.getElementsByClassName('is-video')[0])
        && !(tweet.getElementsByClassName('is-gif')[0])
-       && !(tweet.getElementsByClassName('tooiATimeline')[0])) {
+       && !(tweet.getElementsByClassName('tooiATweetdeckTimeline')[0])) {
         // ボタンを設置
         let origButton = document.createElement('a')
         tweet.getElementsByTagName('footer')[0].appendChild(origButton)
         // tweetdeckのツイート右上の時刻などに使われているclassを使う
-        // 設置の有無の判別用に'tooiATimeline'を付与する
-        origButton.className = 'pull-left margin-txs txt-mute tooiATimeline'
+        // 設置の有無の判別用に'tooiATweetdeckTimeline'を付与する
+        origButton.className = 'pull-left margin-txs txt-mute tooiATweetdeckTimeline'
         // 枠線の色は'Original'と同じく'.txt-mute'の色を使うのでボタンから取得して設定する
         let borderColor = document.defaultView.getComputedStyle(origButton, '').color
         // ボタンのスタイル設定
@@ -107,18 +107,52 @@ function setButtonOnTimeline() {
           paddingRight: '1px'})
         origButton.insertAdjacentHTML('beforeend', 'Original')
         // ボタンを押した時の挙動を設定する
-        origButton.addEventListener('click', openFromTimeline)
+        origButton.addEventListener('click', openFromTweetdeckTimeline)
       }
     })
   }
-} // setButtonOnTimeline end
+} // setButtonOnTweetdeckTimeline end
 
-// // ツイート詳細にボタン表示
-// function setButtonOnTweetDetail() {
-// } // setButtonOnTweetDetail end
+// Tweetdeckのツイート詳細にボタン表示
+function setButtonOnTweetdeckTweetDetail() {
+  // console.log('TODO, ボタン実装') // TODO, debug
+  // if タイムラインのツイートを取得できたら
+  // is-actionable: タイムラインのみ
+  if(document.getElementsByClassName('js-tweet-detail').length != 0) {
+    // 各ツイートに対して
+    Array.from(document.getElementsByClassName('js-tweet-detail'))
+      .map((tweet, i) => {
+      // if メディアツイート
+      // かつ 単数or複数の画像のタグが取得できたら
+      // かつ まだ処理を行っていないなら
+      if( ((tweet.getElementsByClassName('media-img').length != 0) || (tweet.getElementsByClassName('media-image').length != 0))
+        && !(tweet.getElementsByClassName('tooiATweetdeckDetail')[0]) ) {
+        // ボタンを設置
+        let origButton = document.createElement('a')
+        tweet.getElementsByTagName('footer')[0].appendChild(origButton)
+        // tweetdeckのツイート右上の時刻などに使われているclassを使う
+        // 設置の有無の判別用に'tooiATweetdeckDetail'を付与する
+        origButton.className = 'pull-left margin-txs txt-mute tooiATweetdeckDetail'
+        // 枠線の色は'Original'と同じく'.txt-mute'の色を使うのでボタンから取得して設定する
+        let borderColor = document.defaultView.getComputedStyle(origButton, '').color
+        // ボタンのスタイル設定
+        setStyle(origButton,
+          {border: `1px solid ${borderColor}`,
+          borderRadius: '2px',
+          fontSize: '0.75em',
+          marginLeft: '3px',
+          lineHeight: '1.5em',
+          paddingRight: '1px'})
+        origButton.insertAdjacentHTML('beforeend', 'Original')
+        // ボタンを押した時の挙動を設定する
+        origButton.addEventListener('click', openFromTweetdeckTweetDetail)
+      }
+    })
+  }
+} // setButtonOnTweetdeckTweetDetail end
 
-// タイムラインから画像を新しいタブに開く
-function openFromTimeline(e) {
+// Tweetdeckタイムラインから画像を新しいタブに開く
+function openFromTweetdeckTimeline(e) {
   // イベント(MouseEvent)による既定の動作をキャンセル
   e.preventDefault()
   // イベント(MouseEvent)の親要素への伝播を停止
@@ -127,22 +161,42 @@ function openFromTimeline(e) {
   // if 上述のエレメントが取得できたら
   if(this.parentNode.parentNode.parentNode.parentNode.getElementsByClassName('js-media')) {
     openImagesInNewTab(
-      this.parentNode.parentNode.parentNode.parentNode.getElementsByClassName('js-media-image-link')
+      Array.from(this.parentNode.parentNode.parentNode.parentNode.getElementsByClassName('js-media-image-link'))
+        .map(v => {return v.style.backgroundImage})
     )
   }
   else {
-    printException('CANT_FIND_IMAGE_ELEMENT_ON_TIMELINE')
+    printException('CANT_FIND_IMAGE_ELEMENT_ON_TWEETDECK_TIMELINE')
   }
-} // openFromTimeline end
+} // openFromTweetdeckTimeline end
 
-// // ツイート詳細から画像を新しいタブに開く
-// function openFromTweetDetail() {
-// } // openFromTweetDetail end
+// Tweetdeckツイート詳細から画像を新しいタブに開く
+function openFromTweetdeckTweetDetail(e) {
+  // イベント(MouseEvent)による既定の動作をキャンセル
+  e.preventDefault()
+  // イベント(MouseEvent)の親要素への伝播を停止
+  e.stopPropagation()
+  // ツイートの画像の親エレメントを取得するためにその親まで遡る
+  // if 上述のエレメントが取得できたら
+  if(this.parentNode.parentNode.getElementsByClassName('media-img')) {
+    openImagesInNewTab(
+      [this.parentNode.parentNode.getElementsByClassName('media-img')[0].src]
+    )
+  }
+  else if(this.parentNode.parentNode.getElementsByClassName('js-media-image-link')) {
+    openImagesInNewTab(
+      Array.from(this.parentNode.parentNode.getElementsByClassName('js-media-image-link'))
+        .map(v => {return v.style.backgroundImage})
+    )
+  }
+  else {
+    printException('CANT_FIND_IMAGE_ELEMENT_ON_TWEETDECK_DETAIL')
+  }
+} // openFromTweetdeckTweetDetail end
 
 // 画像を原寸で新しいタブに開く
-function openImagesInNewTab(tag) {
-  Array.from(tag).reverse().map((v, i) => {
-    let imgurl = v.style.backgroundImage
+function openImagesInNewTab(imgurls) {
+  Array.from(imgurls).reverse().map(imgurl => {
     // if 画像URLが取得できたなら
     if(!!imgurl) {
       window.open(imgurl.replace(/^[^(]+\(\"(https:[^:]+)(|:[^:]+)\"\)$/, '$1:orig'))
