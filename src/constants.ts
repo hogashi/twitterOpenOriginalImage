32c86f1796ef7def11bded2d948c846e60523a75
hogashi <hogextend@gmail.com>
2019-04-27 18:34:21 +0900

    add constants

---
+++ src/constants.ts
@@ -0,0 +1,31 @@
+// 定数
+
+// 設定取得メッセージ
+export const OPTION_UPDATED    = 'OPTION_UPDATED';
+export const GET_LOCAL_STORAGE = 'GET_LOCAL_STORAGE';
+
+// 公式Web
+export const OPEN_WITH_KEY_PRESS  = 'OPEN_WITH_KEY_PRESS';
+export const SHOW_ON_TIMELINE     = 'SHOW_ON_TIMELINE';
+export const SHOW_ON_TWEET_DETAIL = 'SHOW_ON_TWEET_DETAIL';
+// TweetDeck
+export const SHOW_ON_TWEETDECK_TIMELINE     = 'SHOW_ON_TWEETDECK_TIMELINE';
+export const SHOW_ON_TWEETDECK_TWEET_DETAIL = 'SHOW_ON_TWEETDECK_TWEET_DETAIL';
+// 画像ページ
+export const STRIP_IMAGE_SUFFIX = 'STRIP_IMAGE_SUFFIX';
+
+// 設定
+export const isTrue  = 'istrue';
+export const isFalse = 'isfalse';
+
+export const INITIAL_OPTIONS = {
+  // 公式Web
+  OPEN_WITH_KEY_PRESS: isFalse,
+  SHOW_ON_TIMELINE: isFalse,
+  SHOW_ON_TWEET_DETAIL: isFalse,
+  // TweetDeck
+  SHOW_ON_TWEETDECK_TIMELINE: isFalse,
+  SHOW_ON_TWEETDECK_TWEET_DETAIL: isFalse,
+  // 画像ページ
+  STRIP_IMAGE_SUFFIX: isFalse,
+};
