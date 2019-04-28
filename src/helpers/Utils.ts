bdcd19d4b615f1734bafc8fb9890eed8e17acdc8
hogashi <hogextend@gmail.com>
2019-04-29 01:09:54 +0900

    mv utils

--- src/helpers/Utils.ts
+++ src/helpers/Utils.ts
@@ -1,18 +1,13 @@
-import * as Constants from './constants';
-
-// 設定項目の初期値は「無効」(最初のボタン表示が早過ぎる/一旦表示すると消さないため)
-// 有効だった場合はDOMが変更される間に設定が読み込まれて有効になる
-// 無効だった場合はそのままボタンは表示されない
-const options = Constants.INITIAL_OPTIONS;
+import { GET_LOCAL_STORAGE } from './Constants';
 
 // 設定項目更新
-export const updateOptions = () => {
+export const updateOptions = (options) => {
   console.log('update options: ', options) // debug
   return Promise.all(
     Object.keys(options).map(key =>
       new Promise((resolve, reject) => {
         const request: MessageRequest = {
-          method: Constants.GET_LOCAL_STORAGE,
+          method: GET_LOCAL_STORAGE,
           key,
         };
         const callback = (response: MessageResponse) => {
