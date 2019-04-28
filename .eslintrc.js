5bf70d46f3a8f1f7475dd583b3a2596a53b95941
hogashi <hogextend@gmail.com>
2019-04-29 01:27:20 +0900

    comment-out tsconfig.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
     },
     "ecmaVersion": 2018,
     "sourceType": "module",
-    "project": "./tsconfig.json",
+    // "project": "./tsconfig.json",
   },
   "plugins": [
     "react",
