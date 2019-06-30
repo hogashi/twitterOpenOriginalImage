974f2c3d0b9e3629f38e7ba83bf1c25dd46f7080
hogashi <azuma.ginkakuji@gmail.com>
2019-06-30 15:15:18 +0900

    add test for popup

---
+++ src/popup.test.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import renderer from 'react-test-renderer';
+
+import { OPTIONS_TEXT, INITIAL_OPTIONS, isFalse } from './helpers/Constants';
+import { Popup } from './popup';
+
+describe('Popup', () => {
+  it('render', () => {
+    const optionsText = OPTIONS_TEXT;
+    const optionKeys = Object.keys(INITIAL_OPTIONS);
+    const optionsEnabled = INITIAL_OPTIONS;
+
+    const props = {
+      optionsText,
+      optionKeys,
+      optionsEnabled,
+    };
+
+    window.chrome = {
+      tabs: {
+        query: jest.fn(),
+        sendMessage: jest.fn(),
+      },
+    };
+    const tree = renderer
+      .create(<Popup {...props} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
