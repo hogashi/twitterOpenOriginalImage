ec3129a6744562939f1eb4b2d8f1e20b166ef626
hogashi <azuma.ginkakuji@gmail.com>
2019-07-06 17:09:19 +0900

    fix import, check args

--- __tests__/helpers/ButtonSetter.test.js
+++ __tests__/helpers/ButtonSetter.test.js
@@ -1,5 +1,5 @@
 import ButtonSetter from '../../src/helpers/ButtonSetter';
-import INITIAL_OPTIONS from '../../src/helpers/Constants';
+import { INITIAL_OPTIONS } from '../../src/helpers/Constants';
 
 describe('ButtonSetters', () => {
   describe('setButtonOnTimeline', () => {
@@ -9,7 +9,10 @@ describe('ButtonSetters', () => {
       buttonSetter._setButtonOnReactLayoutTimeline = jest.fn();
 
       buttonSetter.setButtonOnTimeline(INITIAL_OPTIONS);
+
       expect(buttonSetter._setButtonOnTimeline).toHaveBeenCalledTimes(1);
+      expect(buttonSetter._setButtonOnTimeline.mock.calls[0][0]).toStrictEqual(INITIAL_OPTIONS);
+
       expect(buttonSetter._setButtonOnReactLayoutTimeline).not.toHaveBeenCalled();
     });
 
@@ -22,8 +25,11 @@ describe('ButtonSetters', () => {
       root.setAttribute('id', 'react-root');
       document.querySelector('body').appendChild(root);
       buttonSetter.setButtonOnTimeline(INITIAL_OPTIONS);
+
       expect(buttonSetter._setButtonOnTimeline).not.toHaveBeenCalled();
+
       expect(buttonSetter._setButtonOnReactLayoutTimeline).toHaveBeenCalledTimes(1);
+      expect(buttonSetter._setButtonOnReactLayoutTimeline.mock.calls[0][0]).toStrictEqual(INITIAL_OPTIONS);
     });
   });
 });
