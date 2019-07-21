ea68efcded79468092383b5ceac8477be4177907
hogashi <azuma.ginkakuji@gmail.com>
2019-07-21 17:22:09 +0900

    add test for _setButtonOnTimeline of ButtonSetter

--- __tests__/helpers/ButtonSetter.test.js
+++ __tests__/helpers/ButtonSetter.test.js
@@ -1,5 +1,10 @@
 import ButtonSetter from '../../src/helpers/ButtonSetter';
-import { INITIAL_OPTIONS } from '../../src/helpers/Constants';
+import {
+  INITIAL_OPTIONS,
+  SHOW_ON_TIMELINE,
+  isTrue,
+  isFalse,
+} from '../../src/helpers/Constants';
 import { openImages } from '../../src/helpers/Utils';
 
 jest.mock('../../src/helpers/Utils');
@@ -142,5 +147,226 @@ describe('ButtonSetter', () => {
     });
   });
 
-  //   describe('setButtonOnTimeline', () => {
+  describe('_setButtonOnTimeline', () => {
+    /**
+     * @param {string[]} imgSrcs
+     * @param {string[]} extraClassNames
+     * @param {boolean} hasButton
+     */
+    const makeTweet = (imgSrcs, extraElements = [], hasButton = false) => {
+      const root = document.createElement('div');
+      root.classList.add('js-stream-tweet');
+
+      const media = document.createElement('div');
+      media.classList.add('AdaptiveMedia-container');
+
+      const footer = document.createElement('footer');
+      footer.classList.add('ProfileTweet-actionList');
+      if (hasButton) {
+        const button = document.createElement('div');
+        button.classList.add('tooi-button-container-timeline');
+        footer.appendChild(button);
+      }
+
+      imgSrcs.forEach(src => {
+        const div = document.createElement('div');
+        div.classList.add('AdaptiveMedia-photoContainer');
+        const img = document.createElement('img');
+        img.src = src;
+        div.appendChild(img);
+        media.appendChild(div);
+      });
+
+      extraElements.forEach(element => media.appendChild(element));
+
+      root.appendChild(media);
+      root.appendChild(footer);
+      document.body.appendChild(root);
+    };
+
+    beforeEach(() => {
+      document.body.innerHTML = '';
+    });
+
+    it('画像1枚ツイート1つにボタンつけようとする', () => {
+      const imgSrcs = ['https://g.co/img1'];
+      makeTweet(imgSrcs);
+
+      const buttonSetter = new ButtonSetter();
+      buttonSetter.setButton = jest.fn();
+
+      const options = INITIAL_OPTIONS;
+      options[SHOW_ON_TIMELINE] = isTrue;
+      buttonSetter.setButtonOnTimeline(options);
+
+      expect(buttonSetter.setButton).toHaveBeenCalledTimes(1);
+      expect(buttonSetter.setButton.mock.calls[0][0].className).toStrictEqual(
+        'tooi-button-container-timeline'
+      );
+      expect(
+        buttonSetter.setButton.mock.calls[0][0].getImgSrcs()
+      ).toMatchObject(imgSrcs);
+      expect(
+        buttonSetter.setButton.mock.calls[0][0].target.tagName
+      ).toStrictEqual('FOOTER');
+    });
+
+    it('画像1枚ツイート3つにボタンつけようとする', () => {
+      const imgSrcsSet = [
+        ['https://g.co/img1'],
+        ['https://g.co/img2'],
+        ['https://g.co/img3'],
+      ];
+      imgSrcsSet.forEach(imgSrcs => {
+        makeTweet(imgSrcs);
+      });
+
+      const buttonSetter = new ButtonSetter();
+      buttonSetter.setButton = jest.fn();
+
+      const options = INITIAL_OPTIONS;
+      options[SHOW_ON_TIMELINE] = isTrue;
+      buttonSetter.setButtonOnTimeline(options);
+
+      expect(buttonSetter.setButton).toHaveBeenCalledTimes(3);
+      imgSrcsSet.forEach((imgSrcs, index) => {
+        expect(
+          buttonSetter.setButton.mock.calls[index][0].className
+        ).toStrictEqual('tooi-button-container-timeline');
+        expect(
+          buttonSetter.setButton.mock.calls[index][0].getImgSrcs()
+        ).toMatchObject(imgSrcs);
+        expect(
+          buttonSetter.setButton.mock.calls[index][0].target.tagName
+        ).toStrictEqual('FOOTER');
+      });
+    });
+
+    it('画像4枚ツイート3つにボタンつけようとする', () => {
+      const imgSrcsSet = [
+        [
+          'https://g.co/img11',
+          'https://g.co/img12',
+          'https://g.co/img13',
+          'https://g.co/img14',
+        ],
+        [
+          'https://g.co/img21',
+          'https://g.co/img22',
+          'https://g.co/img23',
+          'https://g.co/img24',
+        ],
+        [
+          'https://g.co/img31',
+          'https://g.co/img32',
+          'https://g.co/img33',
+          'https://g.co/img34',
+        ],
+      ];
+      imgSrcsSet.forEach(imgSrcs => {
+        makeTweet(imgSrcs);
+      });
+
+      const buttonSetter = new ButtonSetter();
+      buttonSetter.setButton = jest.fn();
+
+      const options = INITIAL_OPTIONS;
+      options[SHOW_ON_TIMELINE] = isTrue;
+      buttonSetter.setButtonOnTimeline(options);
+
+      expect(buttonSetter.setButton).toHaveBeenCalledTimes(3);
+      imgSrcsSet.forEach((imgSrcs, index) => {
+        expect(
+          buttonSetter.setButton.mock.calls[index][0].className
+        ).toStrictEqual('tooi-button-container-timeline');
+        expect(
+          buttonSetter.setButton.mock.calls[index][0].getImgSrcs()
+        ).toMatchObject(imgSrcs);
+        expect(
+          buttonSetter.setButton.mock.calls[index][0].target.tagName
+        ).toStrictEqual('FOOTER');
+      });
+    });
+
+    it('動画/GIFのツイート1つにボタンつけない', () => {
+      const imgSrcs = [];
+      makeTweet(imgSrcs, [document.createElement('video')]);
+
+      const buttonSetter = new ButtonSetter();
+      buttonSetter.setButton = jest.fn();
+
+      const options = INITIAL_OPTIONS;
+      options[SHOW_ON_TIMELINE] = isTrue;
+      buttonSetter.setButtonOnTimeline(options);
+
+      expect(buttonSetter.setButton).not.toHaveBeenCalled();
+    });
+
+    it('画像1枚ツイート1つでも,もうボタンあったらボタンつけない', () => {
+      const imgSrcs = ['https://g.co/img1'];
+      makeTweet(imgSrcs, [], true);
+
+      const buttonSetter = new ButtonSetter();
+      buttonSetter.setButton = jest.fn();
+
+      const options = INITIAL_OPTIONS;
+      options[SHOW_ON_TIMELINE] = isTrue;
+      buttonSetter.setButtonOnTimeline(options);
+
+      expect(buttonSetter.setButton).not.toHaveBeenCalled();
+    });
+
+    it('ツイート1つあっても画像なかったらボタンつけない', () => {
+      const imgSrcs = [];
+      makeTweet(imgSrcs);
+
+      const buttonSetter = new ButtonSetter();
+      buttonSetter.setButton = jest.fn();
+
+      const options = INITIAL_OPTIONS;
+      options[SHOW_ON_TIMELINE] = isTrue;
+      buttonSetter.setButtonOnTimeline(options);
+
+      expect(buttonSetter.setButton).not.toHaveBeenCalled();
+    });
+
+    it('footerがなかったらボタンつけない', () => {
+      const imgSrcs = ['https://g.co/img1'];
+      makeTweet(imgSrcs);
+
+      const footer = document.querySelector('footer');
+      footer.parentNode.removeChild(footer);
+
+      const buttonSetter = new ButtonSetter();
+      buttonSetter.setButton = jest.fn();
+
+      const options = INITIAL_OPTIONS;
+      options[SHOW_ON_TIMELINE] = isTrue;
+      buttonSetter.setButtonOnTimeline(options);
+
+      expect(buttonSetter.setButton).not.toHaveBeenCalled();
+    });
+
+    it('画像ツイートなかったら何もしない', () => {
+      const buttonSetter = new ButtonSetter();
+      buttonSetter.setButton = jest.fn();
+
+      const options = INITIAL_OPTIONS;
+      options[SHOW_ON_TIMELINE] = isTrue;
+      buttonSetter.setButtonOnTimeline(options);
+
+      expect(buttonSetter.setButton).not.toHaveBeenCalled();
+    });
+
+    it('設定がOFFなら何もしない', () => {
+      const buttonSetter = new ButtonSetter();
+      buttonSetter.setButton = jest.fn();
+
+      const options = INITIAL_OPTIONS;
+      options[SHOW_ON_TIMELINE] = isFalse;
+      buttonSetter.setButtonOnTimeline(options);
+
+      expect(buttonSetter.setButton).not.toHaveBeenCalled();
+    });
+  });
 });
