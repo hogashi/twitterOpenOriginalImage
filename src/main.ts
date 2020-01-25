e0ec136782c84a5ed3e48ebbf632d5f63f24c547
hogashi <hogextend@gmail.com>
2020-01-25 22:39:20 +0900

    add Constants to main

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,69 @@
+/**
+ * Constants */
+// 定数
+
+// 設定取得メッセージ
+const OPTION_UPDATED = 'OPTION_UPDATED';
+const GET_LOCAL_STORAGE = 'GET_LOCAL_STORAGE';
+
+// 公式Web
+const HOST_TWITTER_COM = 'twitter.com';
+const SHOW_ON_TIMELINE = 'SHOW_ON_TIMELINE';
+const SHOW_ON_TWEET_DETAIL = 'SHOW_ON_TWEET_DETAIL';
+// TweetDeck
+const HOST_TWEETDECK_TWITTER_COM = 'tweetdeck.twitter.com';
+const SHOW_ON_TWEETDECK_TIMELINE = 'SHOW_ON_TWEETDECK_TIMELINE';
+const SHOW_ON_TWEETDECK_TWEET_DETAIL = 'SHOW_ON_TWEETDECK_TWEET_DETAIL';
+// 画像ページ
+const HOST_PBS_TWIMG_COM = 'pbs.twimg.com';
+const STRIP_IMAGE_SUFFIX = 'STRIP_IMAGE_SUFFIX';
+
+// 設定
+
+// 設定に使う真偽値
+const isTrue = 'istrue';
+const isFalse = 'isfalse';
+type TooiBoolean = 'istrue' | 'isfalse';
+
+// 設定項目の初期値は「無効」(最初のボタン表示が早過ぎる/一旦表示すると消さないため)
+// 有効だった場合はDOMが変更される間に設定が読み込まれて有効になる
+// 無効だった場合はそのままボタンは表示されない
+interface Options {
+  // 公式Web
+  SHOW_ON_TIMELINE: TooiBoolean;
+  SHOW_ON_TWEET_DETAIL: TooiBoolean;
+  // TweetDeck
+  SHOW_ON_TWEETDECK_TIMELINE: TooiBoolean;
+  SHOW_ON_TWEETDECK_TWEET_DETAIL: TooiBoolean;
+  // 画像ページ
+  STRIP_IMAGE_SUFFIX: TooiBoolean;
+}
+type OptionsMaybe = { [key in keyof Options]?: TooiBoolean };
+const INITIAL_OPTIONS: Options = {
+  // 公式Web
+  SHOW_ON_TIMELINE: isFalse,
+  SHOW_ON_TWEET_DETAIL: isFalse,
+  // TweetDeck
+  SHOW_ON_TWEETDECK_TIMELINE: isFalse,
+  SHOW_ON_TWEETDECK_TWEET_DETAIL: isFalse,
+  // 画像ページ
+  STRIP_IMAGE_SUFFIX: isFalse,
+};
+const OPTIONS_TEXT: { [key: string]: string } = {
+  // 公式Web
+  SHOW_ON_TIMELINE: 'タイムラインにボタンを表示',
+  SHOW_ON_TWEET_DETAIL: 'ツイート詳細にボタンを表示',
+  // TweetDeck
+  SHOW_ON_TWEETDECK_TIMELINE: 'タイムラインにボタンを表示',
+  SHOW_ON_TWEETDECK_TWEET_DETAIL: 'ツイート詳細にボタンを表示',
+  // 画像ページ
+  STRIP_IMAGE_SUFFIX: '[Ctrl]+[s]で拡張子を校正',
+};
+
+/**
+ * main */
+
 import ButtonSetters from './helpers/ButtonSetters';
-import {
-  INITIAL_OPTIONS,
-  OPTION_UPDATED,
-  Options,
-  TooiBoolean,
-} from './helpers/Constants';
 import { getOptions } from './helpers/Utils';
 
 // 実行の間隔(ms)
