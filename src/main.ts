c594df664ccb1d62756f1b14b9bd99d3f00410e8
hogashi <hogextend@gmail.com>
2020-01-25 22:52:00 +0900

    add Utils to main

--- src/main.ts
+++ src/main.ts
@@ -62,10 +62,134 @@ const OPTIONS_TEXT: { [key: string]: string } = {
 };
 
 /**
+ * Utils
+ */
+import { MessageRequest, MessageResponse } from '../background';
+
+// エラーメッセージの表示(予期せぬ状況の確認)
+const printException = (tooiException: string) => {
+  console.log('tooi: ' + tooiException + ' at: ' + window.location.href);
+};
+
+// 画像urlの要素を集める
+const collectUrlParams = (
+  rawUrl: string
+): {
+  protocol: string;
+  host: string;
+  pathname: string;
+  format: string;
+  name: string | null;
+} | null => {
+  if (!/https:\/\/pbs\.twimg\.com\/media/.test(rawUrl)) {
+    // twitterの画像URLでないときnull
+    return null;
+  }
+
+  const url = new URL(rawUrl);
+  const searchSet: {
+    format: string;
+    name: string | null;
+  } = {
+    format: 'jpg', // 拡張子が無い場合はjpgにフォールバック
+    name: null, // 大きさ指定がない場合はnull
+  };
+  url.search
+    .slice(1)
+    .split('&')
+    .forEach(set => {
+      const [key, value] = set.split('=');
+      if (key === 'format' || key === 'name') {
+        searchSet[key] = value;
+      }
+    });
+
+  // 空文字でもどんな文字列でもマッチする正規表現なのでnon-null
+  const matched = url.pathname.match(/^(.*?)(?:|\.([^.:]+))(?:|:([a-z]+))$/)!;
+  // どんな文字列でも空文字は最低入るのでnon-null
+  const pathnameWithoutExtension = matched[1]!;
+  // 拡張子はないかもしれないのでundefinedも示しておく
+  const extension = matched[2] as string | undefined;
+  // コロンを使う大きさ指定はないかもしれないのでなかったらnull
+  const legacyName = matched[3] || null;
+
+  return {
+    protocol: url.protocol,
+    host: url.host,
+    pathname: pathnameWithoutExtension,
+    // 2.1.11時点ではクエリパラメータを使うのはTweetDeckのみ
+    // TweetDeckのURLでは拡張子を優先する
+    // ref: https://hogashi.hatenablog.com/entry/2018/08/15/042044
+    format: extension || searchSet.format,
+    name: searchSet.name || legacyName,
+  };
+};
+
+// 画像URLを https～?format=〜&name=orig に揃える
+const formatUrl = (imgUrl: string | null) => {
+  if (!imgUrl || imgUrl.length === 0) {
+    return null;
+  }
+
+  const params = collectUrlParams(imgUrl);
+  if (!params) {
+    // twitterの画像URLでないときそのまま返す
+    return imgUrl;
+  }
+
+  const { protocol, host, pathname, format } = params;
+  return `${protocol}//${host}${pathname}?format=${format}&name=orig`;
+};
+
+// 画像を開く
+const openImages = (imgSrcs: (string | null)[]) => {
+  if (imgSrcs.length === 0) {
+    printException('zero image urls');
+    return;
+  }
+  Array.from(imgSrcs)
+    .reverse() // 逆順に開くことで右側のタブから読める
+    .forEach(imgSrc => {
+      // if 画像URLが取得できたなら
+      const url = formatUrl(imgSrc);
+      if (url) {
+        window.open(url);
+      } else {
+        printException('no image url');
+      }
+    });
+};
+
+// 設定項目更新
+const getOptions = () => {
+  console.log('update options'); // debug
+  return new Promise((resolve, reject) => {
+    const request: MessageRequest = {
+      method: GET_LOCAL_STORAGE,
+    };
+    const callback = (response: MessageResponse) => {
+      if (response.data) {
+        resolve(response.data);
+      } else {
+        reject();
+      }
+    };
+    chrome.runtime.sendMessage(request, callback);
+  }).then((data: OptionsMaybe) => {
+    const options: { [key: string]: string } = {};
+    Object.keys(INITIAL_OPTIONS).map((key: keyof Options) => {
+      options[key] = data[key] || isTrue;
+    });
+
+    console.log('update options: ', options); // debug
+
+    return options;
+  });
+};
+
+/**
  * ButtonSetter
  */
-import { openImages, printException } from './Utils';
-
 // twitter.comでボタンを設置するクラス
 
 class ButtonSetter {
@@ -388,8 +512,6 @@ class ButtonSetter {
 /**
  * ButtonSetterTweetDeck
  */
-import { printException } from './Utils';
-
 // tweetdeck.twitter.comでボタンを設置するクラス
 
 class ButtonSetterTweetDeck extends ButtonSetter {
@@ -581,8 +703,6 @@ ButtonSetters[HOST_TWEETDECK_TWITTER_COM] = new ButtonSetterTweetDeck();
 /**
  * main
  */
-import { getOptions } from './helpers/Utils';
-
 // 実行の間隔(ms)
 const INTERVAL = 300;
 
