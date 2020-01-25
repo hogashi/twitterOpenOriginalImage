f62fdbb37eb0a8cf8e70ce25b617e4862d8b8391
hogashi <hogextend@gmail.com>
2020-01-26 01:52:04 +0900

    prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,45 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true,
-    "jest/globals": true,
+  env: {
+    browser: true,
+    es6: true,
+    'jest/globals': true,
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:jest/recommended",
-    "plugin:prettier/recommended",
-    "prettier/react",
-    "prettier/standard",
-    "prettier/@typescript-eslint",
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended',
+    'prettier/react',
+    'prettier/standard',
+    'prettier/@typescript-eslint',
   ],
-  "globals": {
-    "Atomics": "readonly",
-    "SharedArrayBuffer": "readonly",
-    "chrome": "readonly",
-    "window": true,
-    "document": "readonly",
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    chrome: 'readonly',
+    window: true,
+    document: 'readonly',
   },
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaFeatures": {
-        "jsx": true
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "ecmaVersion": 2018,
-    "sourceType": "module",
-    "project": "./tsconfig.json",
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
   },
-  "plugins": [
-    "react",
-    "@typescript-eslint",
-    "jest",
-  ],
-  "rules": {
-    "no-console": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "react/jsx-uses-react": [1],
+  plugins: ['react', '@typescript-eslint', 'jest'],
+  rules: {
+    'no-console': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'react/jsx-uses-react': [1],
   },
-  "settings": {
-    "react": {
-      "version": "detect",
+  settings: {
+    react: {
+      version: 'detect',
     },
   },
 };
