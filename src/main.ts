19d7d38afd07d31467127320a7e1ec92987b775c
hogashi <hogextend@gmail.com>
2020-01-26 16:25:23 +0900

    insert blank line

--- src/main.ts
+++ src/main.ts
@@ -45,6 +45,7 @@ export const SHOW_ON_TWEETDECK_TWEET_DETAIL = 'SHOW_ON_TWEETDECK_TWEET_DETAIL';
 // 画像ページ
 export const HOST_PBS_TWIMG_COM = 'pbs.twimg.com';
 export const STRIP_IMAGE_SUFFIX = 'STRIP_IMAGE_SUFFIX';
+
 const hostname = window.location.hostname;
 
 // 公式Webかどうか
