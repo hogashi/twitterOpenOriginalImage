e09ec2c9d9832868fab7b275f8488d335c37a59b
hogashi <azuma.ginkakuji@gmail.com>
2021-06-11 22:47:05 +0900

    error only in src

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,10 +28,18 @@ module.exports = {
   plugins: ['react', '@typescript-eslint', 'jest'],
   rules: {
     'no-console': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     'react/react-in-jsx-scope': 'off',
   },
+  overrides: [
+    {
+      files: ['src/**/*.ts', 'src/**/*.tsx'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 'error',
+      },
+    },
+  ],
   settings: {
     react: {
       version: 'detect',
