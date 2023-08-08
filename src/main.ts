da8ca0b1340a2afc4f1391effd3b3784a65577e5
hogashi <azuma.ginkakuji@gmail.com>
2023-08-08 14:14:17 +0900

    fixFileNameOnSaveCommand no longer needed when format/name query parameters, not :orig

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
-import { isImageTab, isTweetdeck, isTwitter } from './constants';
-import { fixFileNameOnSaveCommand, setOriginalButton, updateOptions } from './utils';
+import { isTweetdeck, isTwitter } from './constants';
+import { setOriginalButton, updateOptions } from './utils';
 
 updateOptions().then((options) => {
   if (isTwitter() || isTweetdeck()) {
     /** 公式Web/TweetDeck */
     setOriginalButton(options);
-  } else if (isImageTab()) {
-    /** 画像ページ(https://pbs.twimg.com/*) */
-    fixFileNameOnSaveCommand(options);
   }
 });
