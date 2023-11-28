f40693c32d4a29bfb8cf0bf1a7b8460c597a6199
hogashi <azuma.ginkakuji@gmail.com>
2023-11-29 04:58:37 +0900

    add test

--- src/extension-contexts/popup.tsx
+++ src/extension-contexts/popup.tsx
@@ -119,7 +119,7 @@ export const Popup = (props: Props): JSX.Element => {
               type="text"
               value={enabled[ORIGINAL_BUTTON_TEXT_OPTION_KEY]}
               onChange={onOriginalButtonTextInputChange}
-              className="block w-1/2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2 text-base"
+              className={`${ORIGINAL_BUTTON_TEXT_OPTION_KEY} block w-1/2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2 text-base`}
             />
           </div>
         </div>
